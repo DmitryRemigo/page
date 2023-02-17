d00fc697473b30341a25c4704ecf01cfad3a738d
dmitry <120300724+DmitryRemigo@users.noreply.github.com>
2023-02-17 12:44:03 +0400

    Add files via upload

---
+++ pageTwo.js
@@ -0,0 +1,6 @@
+const button14022 = document.querySelector('.button_1402_2');
+const element14022 = document.querySelector('.element_1402_2');
+function button14022F(){
+	button14022.classList.toggle('active');
+	};
+button14022.addEventListener('click' , button14022F);
